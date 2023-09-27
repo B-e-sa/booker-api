441086a20108481b4fcc008afd6082ccc5672819
Besa <joao.pereira@precato.com.br>
2023-09-27 15:11:03 -0300

    feat: initing database setup

---
+++ Data/BookerDbContext.cs
@@ -0,0 +1,17 @@
+using Booker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booker.Data
+{
+    public class BookerDbContext : DbContext
+    {
+        public BookerDbContext(DbContextOptions<BookerDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book>? Books { get; set; }
+        public DbSet<Genre>? Genres { get; set; }
+        public DbSet<Publisher>? Publishers { get; set; }
+    }
+}
