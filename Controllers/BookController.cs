d346bf20853b821d37aa9b5a3d30536014f69440
Besa <joao.pereira@precato.com.br>
2023-09-27 15:02:24 -0300

    feat: initing app controllers

---
+++ Controllers/BookController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booker.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    class Book : ControllerBase
+    {
+        
+    }
+}
---
+++ Controllers/PublisherController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Booker.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    class Publisher : ControllerBase
+    {
+        
+    }
+}
