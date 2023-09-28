8e507ffc0b35d9eb2a6bcdd5aadc21edd191e75c
Besa <joao.pereira@precato.com.br>
2023-09-28 19:07:30 -0300

    update: adding all current methods to book service

--- Services/Implementations/BookService.cs
+++ Services/Implementations/BookService.cs
@@ -1,9 +1,24 @@
+using Booker.Models;
+using Booker.Repositories.Implementations;
+using Booker.Repositories.Models;
 using Booker.Services.Models;
 
 namespace Booker.Services.Implementations
 {
     public class BookService : IBookService
     {
+        private readonly IBookRepository _bookRepository;
 
+        public BookService(IBookRepository bookRepository) => _bookRepository = bookRepository;
+
+        public async Task<Book?> FindById(string id) => await _bookRepository.FindById(id);
+
+        public async Task<Book> Add(Book book) => await _bookRepository.Add(book);
+
+        public async Task<List<Book>> FindAll() => await _bookRepository.FindAll();
+
+        public async Task<Book> Update(string id) => await _bookRepository.Update(id);
+
+        public async Task<Book> Delete(string id) => await _bookRepository.Delete(id);
     }
 }
--- Services/Models/IBookService.cs
+++ Services/Models/IBookService.cs
@@ -1,7 +1,13 @@
+using Booker.Models;
+
 namespace Booker.Services.Models
 {
     public interface IBookService
     {
-
+        Task<Book> Add(Book book);
+        Task<Book?> FindById(string id);
+        Task<List<Book>> FindAll();
+        Task<Book> Update(string id);
+        Task<Book> Delete(string id);
     }
 }
