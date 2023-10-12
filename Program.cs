448e5c53c473a98f50129045b23cd1f189fcb32e
B-e-sa <joaospereira963@gmail.com>
2023-10-12 00:31:14 -0300

    fix: fixing non https localhost

--- Program.cs
+++ Program.cs
@@ -1,16 +1,24 @@
 using Booker.Data;
+using Booker.Repositories.Implementations;
+using Booker.Repositories.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var service = builder.Services;
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<BookerDbContext>(options =>
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("Database")
-    )
-);
+service.AddControllers();
+service.AddEndpointsApiExplorer();
+service.AddSwaggerGen();
+
+service.AddSingleton<IBookRepository, BookRepository>();
+service.AddSingleton<IAuthorRepository, AuthorRepository>();
+service.AddSingleton<IPublisherRepository, PublisherRepository>();
+service.AddSingleton<IGenreRepository, GenreRepository>();
+
+service.AddDbContext<BookerDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Database"));
+}, ServiceLifetime.Scoped);
 
 var app = builder.Build();
 
