f3a6a308535fee98f15bd1563247539540f3299f
B-e-sa <joaospereira963@gmail.com>
2023-10-31 02:09:20 -0300

    update: add update, delete and find by id methods to book repository

--- Repositories/Models/IBookRepository.cs
+++ Repositories/Models/IBookRepository.cs
@@ -5,9 +5,9 @@ namespace Booker.Repositories.Models
     public interface IBookRepository
     {
         Task<Book> Add(Book book);
-        Task<Book?> FindById(string id);
+        Task<Book?> FindById(Guid id);
         Task<List<Book>> FindAll();
-        Task<Book> Update(string id);
-        Task<Book> Delete(string id);
+        Task<Book> Update(Guid id);
+        Task<Book> Delete(Guid id);
     }
 }
