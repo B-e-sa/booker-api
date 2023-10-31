293b050d8c216597e01e79f66c40273bc5ef5792
B-e-sa <joaospereira963@gmail.com>
2023-10-31 02:09:40 -0300

    update: add update, delete and find by id methods to book service

--- Services/Implementations/BookService.cs
+++ Services/Implementations/BookService.cs
@@ -11,14 +11,14 @@ namespace Booker.Services.Implementations
 
         public BookService(IBookRepository bookRepository) => _bookRepository = bookRepository;
 
-        public async Task<Book?> FindById(string id) => await _bookRepository.FindById(id);
+        public async Task<Book?> FindById(Guid id) => await _bookRepository.FindById(id);
 
         public async Task<Book> Add(Book book) => await _bookRepository.Add(book);
 
         public async Task<List<Book>> FindAll() => await _bookRepository.FindAll();
 
-        public async Task<Book> Update(string id) => await _bookRepository.Update(id);
+        public async Task<Book> Update(Guid id) => await _bookRepository.Update(id);
 
-        public async Task<Book> Delete(string id) => await _bookRepository.Delete(id);
+        public async Task<Book> Delete(Guid id) => await _bookRepository.Delete(id);
     }
 }
