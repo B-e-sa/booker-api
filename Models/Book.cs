6a5b611e934c8e390d8d14175641a491e0889d3d
B-e-sa <joaospereira963@gmail.com>
2023-11-05 00:45:41 -0300

    update: switch book required attributes

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,8 +8,10 @@ namespace Booker.Models
     {
         public Guid? Id { get; set; }
 
+        [Required]
         public Guid? AuthorId { get; set; }
 
+        [Required]
         public Guid? PublisherId { get; set; }
 
         [Required]
@@ -21,14 +23,15 @@ namespace Booker.Models
         [Required]
         [RegularExpression(
             @"^\d{3}-\d-\d{2}-\d{6}-\d$",
-            ErrorMessage = "Invalid ISBN format"
+            ErrorMessage = "Invalid ISBN format (must be xxx-x-xx-xxxxxx-x)"
         )]
-        public string? ISBN  { get; set; }
+        public string? ISBN { get; set; }
 
         public Author? Author { get; set; }
 
         public Publisher? Publisher { get; set; }
 
+        [Required]
         public ICollection<Genre>? Genres { get; set; }
     }
 }
