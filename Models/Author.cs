0d3d56427897c462db1c97a22479a4d7f051bda9
B-e-sa <joaospereira963@gmail.com>
2023-11-08 13:53:56 -0300

    update: add line breaks for redability

--- Models/Author.cs
+++ Models/Author.cs
@@ -5,12 +5,16 @@ namespace Booker.Models
     public class Author
     {
         public Guid Id { get; set; }
+
         [Required]
         [StringLength(30)]
         public string? FirstName { get; set; }
+
         [StringLength(30)]
         public string? LastName { get; set; }
+
         public ICollection<Genre>? Genres { get; set; }
+        
         public ICollection<Book>? Books { get; set; }
     }
 }
--- Models/Publisher.cs
+++ Models/Publisher.cs
@@ -3,11 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Booker.Models
 {
-     [Index(nameof(Name), IsUnique = true)]
+    [Index(nameof(Name), IsUnique = true)]
     public class Publisher
     {
         public Guid? Id { get; set; }
+
+        [Required]
         [StringLength(30)]
         public string? Name { get; set; }
+        
+        public ICollection<Book>? Books { get; set; }
     }
 }
