1be44e2db3876bb708b3b309c1c588ab67f10089
B-e-sa <joaospereira963@gmail.com>
2023-11-30 20:20:02 -0300

    update: removing nullable Id attribute from publisher

--- Models/Publisher.cs
+++ Models/Publisher.cs
@@ -6,12 +6,12 @@ namespace Booker.Models
     [Index(nameof(Name), IsUnique = true)]
     public class Publisher
     {
-        public Guid? Id { get; set; }
+        public Guid Id { get; set; }
 
         [Required]
         [StringLength(30)]
         public string? Name { get; set; }
         
-        public List<Book> Books { get; } = new();
+        public List<Book> Books { get; set; } = new();
     }
 }
